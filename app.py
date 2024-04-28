45794b900fd162cae834c30bdbe4e9473eeadbbb
Baljeet942000 <154469608+Baljeet942000@users.noreply.github.com>
2024-04-28 20:51:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import streamlit as st
+! pip install xgboost
 from xgboost import XGBRegressor
 
 # loading xgboost model and standard scaler.
